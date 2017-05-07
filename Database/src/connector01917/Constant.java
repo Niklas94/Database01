bbaea9a6b95b09d29f1692e6431eeff609229a54
Einar Logi <einarlogi1@gmail.com>
2017-05-07 15:02:21 +0200

    established connectin to database

--- Database/src/connector01917/Constant.java
+++ Database/src/connector01917/Constant.java
@@ -6,10 +6,10 @@ package connector01917;
 public abstract class Constant
 {
 	public static final String
-		server					= "sql-lab1.cc.dtu.dk",  // database-serveren
-		database				=  "s145032",  //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "s145032", // dit brugernavn = dit studienummer 
-		password				= ""; // dit password som du har valgt til din database
+		server					= "localhost",  // database-serveren
+		database				=  "project",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "EinarLogi", // dit brugernavn = dit studienummer 
+		password				= "qwe123"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
